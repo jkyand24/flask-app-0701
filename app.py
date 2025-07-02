475be585bcd015eedcca40121a389e9cd88c95b7
jkyand24 <jkyand24@gmail.com>
2025-07-02 10:25:42 +0900

    어떤 수정

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, Jenkins!'
+    return '이것이 보인다면 수정사항이 정상적으로 반영된 것'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=4000)
